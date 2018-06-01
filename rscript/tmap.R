de7d35e9419a840193641611244a48b1684e03f1
liao <liaomovie2@gmail.com>
2018-06-02 00:12:17 +0800

    tmap test

--- rscript/tmap.R
+++ rscript/tmap.R
@@ -1,6 +1,7 @@
 library(tmap)
 library(tmaptools)
 library(tidyverse)
+library(showtext)
 
 source("shape.R")
 
@@ -9,9 +10,12 @@ source("shape.R")
 
 #tm_shape(sp) + tm_fill(col="COUNTYNAME", title="臺灣各縣市")
 
+showtext.auto(enable = TRUE)
+font.add("NotoSansCJK", "NotoSansCJKtc.otf")
+
 map <- tm_shape(sp) + 
     tm_polygons(col="COUNTYNAME") +
     tm_layout(title = "臺灣鄉鎮市區",
               bg.color = "grey85",
               frame = FALSE,
-              fontfamily = "PingFang TC")
+              fontfamily = "NotoSansCJK")
