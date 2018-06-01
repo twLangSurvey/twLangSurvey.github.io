59d5be3aa8086961f3b3b4858fbc3419f2932ac6
liao <liaomovie2@gmail.com>
2018-06-01 21:02:30 +0800

    up

--- rscript/shape.R
+++ rscript/shape.R
@@ -18,3 +18,11 @@ sp <- sp %>%
 animation::saveGIF(for (i in colnames(sp)[c(2,3)]) plot(sp[i]),
         movie.name = "../web_source/out_graph/taiwan_sp.gif", img.name = "plott",
         ani.dev = function(...){png(res=130*1.2,...)})
+
+
+#png(filename = "taiwan.png",width = 512, height = 512, units = "px", pointsize = 1,bg = "transparent", type = "cairo", res=300)
+#svg(file = "taiwan.svg", width = 12, height = 12, onefile = TRUE,bg = "transparent")
+library(maps)
+#map("world", fill=TRUE, col="white", bg="transparent", ylim=c(20, 26),xlim=c(120, 124), mar=c(0,0,0,0))
+#plot(sp[,-(1:3)])
+#dev.off() 
